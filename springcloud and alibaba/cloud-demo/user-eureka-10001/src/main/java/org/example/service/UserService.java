03c800ab77b4778c805b213587948a471ec4c01c
cpy <66666666@qq.com>
2023-11-03 20:07:37 +0800

    ok

--- springcloud and alibaba/cloud-demo/user-eureka-10001/src/main/java/org/example/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package org.example.service;
-
-import org.example.entity.User;
-
-public interface UserService {
-    public User getUser(Long id);
-}
--- springcloud-springcloudalibaba/cloud-demo/user-service/src/main/java/org/example/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package org.example.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.example.entity.User;
-@Mapper
-public interface UserMapper {
-    User getUserById(@Param("id")Long id);
-}
