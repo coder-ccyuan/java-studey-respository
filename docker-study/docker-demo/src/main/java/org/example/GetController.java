f70b46c3c7f4a7b8dc8c671c4cf39fb92744452e
cpy <66666666@qq.com>
2023-11-06 22:33:17 +0800

    提交

--- docker-study/docker-demo/src/main/java/org/example/GetController.java
+++ docker-study/docker-demo/src/main/java/org/example/GetController.java
@@ -2,16 +2,25 @@ package org.example;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.Date;
 @RestController
 @RequestMapping("/get")
 public class GetController {
     @GetMapping("/getNow")
-    public String getNow(){
+    public String getNow(HttpServletRequest request){
         Date date = new Date();
-        return date.toString();
+        HttpSession session = request.getSession();
+        if (session.getAttribute("count")==null){
+            session.setAttribute("count",1);
+        }else{
+            int count =(Integer) session.getAttribute("count");
+            count++;
+            session.setAttribute("count",count);
+        }
+        return "调用get/getNow"+(int)session.getAttribute("count")+"次";
     }
 }
