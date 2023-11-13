d227a8de05fb36feabc001054b324ecf8aa8ef76
cpy <66666666@qq.com>
2023-11-13 20:28:34 +0800

    09-UV统计 a

---
+++ java-web/tomcat-init/src/main/java/servlet/Myservlet.java
@@ -0,0 +1,32 @@
+package servlet;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class Myservlet implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        System.out.println("init");
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        System.out.println("config");
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("service");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy");
+    }
+}
